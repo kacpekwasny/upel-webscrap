5fc4477f3ffe15dd203c312f1538318b649e686f
Kacper Kwasny <kwasny.it@gmail.com>
2022-02-12 00:40:01 +0100

    commit

--- discord_main.py
+++ discord_main.py
@@ -4,8 +4,8 @@ import discord
 from datetime import timedelta
 
 
-from secrets import DISCORD_TOKEN, USERNAME, PASSWORD
-from constatns import LOGIN_URL
+from secrets import DISCORD_TOKEN, USERNAME, PASSWORD, POPULAR_IDS
+from constants import LOGIN_URL
 
 from lib.discord.follow_user_online import FollowUpelUser
 from lib.discord.firewall import Firewall
@@ -36,7 +36,7 @@ async def on_message(message):
 
     if "-help" == message.content[:5]:
         await message.channel.send(
-"""
+f"""
 You can follow UPEL users to be notified on this chat/channel when they become online / offline.
 Do this by entering a command: `-follow wxyz` where `wxyz` is the `...profile.php?id=wxyz` in url in the UPEL profile page.
 To unfollow execute command: `-unfollow wxyz`
@@ -51,8 +51,11 @@ If you see a message indicating success then you can proceed with the next step:
 or `-getgrades wxyz` example: `-getgrades 1477` beacuse 1477 is the course id of Podstawy Telekomunikacji.
 The id can be found in the URL the same way as id of UPEL profile id.
 
+{POPULAR_IDS}
+
 The source code for this bot can be found here: https://github.com/kacpekwasny/upel-webscrap/tree/discord
 """
+        # POPULAR_IDS is rather self explanatory, but i do not want to share lastnames publicly
         )
 
     if fuu.is_command(message.content):
